898d91ad93b99082315a67d9d4dfa4357d14ba43
formifan2002 <d.behrendt@web.de>
2026-03-02 19:05:56 +0100

    Push to cloudsmith

--- github/github-build-ubuntu-noble.sh
+++ github/github-build-ubuntu-noble.sh
@@ -1,4 +1,4 @@
-﻿#!/usr/bin/env bash
+#!/usr/bin/env bash
 set -e
 
 echo "=== Building OpenCPN Plugin for Ubuntu 24.04 (Noble) x86_64 ==="
@@ -8,20 +8,30 @@ docker run --rm \
   -w /workspace \
   ubuntu:24.04 \
   bash -c "
+    set -e
+
     apt-get update &&
     apt-get install -y \
       cmake g++ make git \
       libwxgtk3.2-dev wx-common \
       libglu1-mesa-dev freeglut3-dev mesa-common-dev \
       libcurl4-openssl-dev \
-      libtinyxml-dev \
+      libtinyxml2-dev \
       libarchive-dev \
       zlib1g-dev \
-      opencpn-plugin-dev &&
+      pkg-config
+
+    echo '=== Cloning OpenCPN Plugin SDK ==='
+    git clone --depth=1 https://github.com/OpenCPN/OpenCPN.git /tmp/opencpn
 
     mkdir -p build &&
     cd build &&
-    cmake -DCMAKE_BUILD_TYPE=Release .. &&
+    cmake \
+      -DOCPN_PLUGIN=ON \
+      -DwxWidgets_CONFIG_EXECUTABLE=/usr/bin/wx-config \
+      -DOCPN_SDK_PATH=/tmp/opencpn \
+      -DCMAKE_BUILD_TYPE=Release \
+      .. &&
     make -j\$(nproc)
   "
 
